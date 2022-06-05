1572d46961d54918f5bc1b1f077764f5bcb7b430
axelcerkvenih <cerkvenihaxel@gmail.com>
2022-06-04 22:28:38 -0300

    Added filesystem api

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 //  * Serie de Fibonacci
 // * 1 1 2 3 5 8 13 21 34 ... 
+const fs = require('fs');
 
 let fibo1 = 1;
 let fibo2 = 1; 
+let serie=[];
 
 console.log(`${fibo1}`);
 
 for(let i=2; i<=10; i++){
-    console.log(`${fibo2}`);
+    serie+=`${fibo2}\n`;
     fibo2=fibo1+fibo2;
     fibo1=fibo2-fibo1;
+    console.log(`${fibo2}`);
 }
+
+fs.writeFile('message.txt', serie, (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+  });  // end of fs.writeFile
+ 
