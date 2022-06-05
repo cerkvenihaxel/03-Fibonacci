05fcd74a8c3a73f1d0fdfa578907952f0e2d56fa
axelcerkvenih <cerkvenihaxel@gmail.com>
2022-06-04 22:44:12 -0300

    Created a module named crearSerie and used promise

--- app.js
+++ app.js
@@ -1,22 +1,5 @@
-//  * Serie de Fibonacci
-// * 1 1 2 3 5 8 13 21 34 ... 
-const fs = require('fs');
+const serie = require('./serie');
 
-let fibo1 = 1;
-let fibo2 = 1; 
-let serie=[];
-
-console.log(`${fibo1}`);
-
-for(let i=2; i<=10; i++){
-    serie+=`${fibo2}\n`;
-    fibo2=fibo1+fibo2;
-    fibo1=fibo2-fibo1;
-    console.log(`${fibo2}`);
-}
-
-fs.writeFile('message.txt', serie, (err) => {
-    if (err) throw err;
-    console.log('The file has been saved!');
-  });  // end of fs.writeFile
- 
+serie.crearSerie()
+    .then(message => console.log(message))
+    .catch(err => console.log(err));
---
+++ serie.js
@@ -0,0 +1,31 @@
+//  * Serie de Fibonacci
+// * 1 1 2 3 5 8 13 21 34 ... 
+const fs = require('fs');
+
+let crearSerie = () => {
+
+    return new Promise((resolve, reject) => {
+        let fibo1 = 1;
+        let fibo2 = 1;
+        let serie = [];
+
+        console.log(`${fibo1}`);
+
+        for (let i = 2; i <= 10; i++) {
+            serie += `${fibo2}\n`;
+            fibo2 = fibo1 + fibo2;
+            fibo1 = fibo2 - fibo1;
+            console.log(`${fibo2}`);
+        }
+
+        fs.writeFile('message.txt', serie, (err) => {
+            if (err)
+                reject('Error al crear el archivo.');
+            else
+                resolve('Archivo creado con éxito.');
+        }); // end of fs.writeFile   */  
+    }); // end of Promise
+}
+module.exports = {
+    crearSerie
+}
